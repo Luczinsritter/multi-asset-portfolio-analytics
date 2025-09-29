51bd74a5dd8e654510ab54c151a1f5495c69c2a2
Luczinsritter <luc.zins-ritter@edu.dsti.institute>
2025-09-29 19:42:05 +0200

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -31,9 +31,9 @@ class FinancialData:
     @staticmethod
     def clean_data(df):
         if isinstance(df.columns, pd.MultiIndex):
-            if "Close" in df.columns.get_level_values(0):
+            if 'Close' in df.columns.get_level_values(0):
                 df = df['Close']
-            df.columns = [f"{c}_Close" for c in df.columns]
+            df.columns = [f'{c}_Close' for c in df.columns]
             return df.sort_index().ffill().dropna(how='any')
         cols = [c for c in ['Open', 'High', 'Low', 'Close', 'Volume'] if c in df.columns]
         return df[cols].sort_index().ffill().dropna(how='any')
@@ -85,10 +85,10 @@ class ChosenTickers:
 
 
 class Portfolio(ChosenTickers):
-    """
+    '''
     Portfolio class for creating a portfolio object,running optimisation, computing portfolio value series,
     and plotting the efficient frontier
-    """
+    '''
 
     def __init__(self, tickers_list: list[str], end_date, duration: int, chosen_interval: str,
                  capital: float, allow_short: bool = False):
@@ -99,15 +99,15 @@ class Portfolio(ChosenTickers):
         self.value_series = None
 
     def optimized_pf(self):
-        """
+        '''
         Compute the portfolio value series using the efficient frontier
-        """
+        '''
         if self.weights is None:
             self.efficient_frontier_with_alpha(alpha=0.5)
         prices_df = self.download_all_close()
-        ordered_cols = [f"{t}_Close" for t in self.tickers_list]
+        ordered_cols = [f'{t}_Close' for t in self.tickers_list]
         prices_df = prices_df[ordered_cols]
-        w = np.array([self.weights[f"{t}_log_returns"] if f"{t}_log_returns" in self.weights else self.weights[t]
+        w = np.array([self.weights[f'{t}_log_returns'] if f'{t}_log_returns' in self.weights else self.weights[t]
                       for t in self.tickers_list])
         norm_px = prices_df / prices_df.iloc[0]
         total_value = self.capital * (norm_px.to_numpy() @ w)
@@ -118,12 +118,12 @@ class Portfolio(ChosenTickers):
 
     def efficient_frontier_with_alpha(self, num_points: int = 50, use_log_returns=True,
                                    risk_free_rate_annual=0.0, alpha: float = 1.0):
-        """
+        '''
         Compute the efficient frontier and select a portfolio based on alpha:
         alpha = 1.0 -> Max Sharpe
         alpha = 0.0 -> Min Volatility
         0 < alpha < 1 -> Compromise between Sharpe and low volatility
-        """
+        '''
         R = self.download_all_log_returns() if use_log_returns else self.download_all_simp_returns()
         mu_per = R.mean().to_numpy()
         cov_per = R.cov().to_numpy()
